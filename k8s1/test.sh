7cfb9f48cfefdd4d54724691d4589d2c85539621
Alexandr <IamWebDev@gmail.com>
2025-08-25 11:17:04 -0400

    p1 clean up

--- k8s1/test.sh
+++ k8s1/test.sh
@@ -5,10 +5,6 @@
 
 echo "--- Starting test: Sending 10 requests to the nginx service ---"
 
-kubectl run my-proxy \
-  --image=busybox \
-  -it \
-  --rm \
-  -- sh -c 'sleep 3;for i in $(seq 1 10); do wget -q -O - http://nginx | grep "<h1>"; done'
+kubectl run my-proxy --image=busybox -it --rm -- sh -c 'sleep 1;for i in $(seq 1 10); do wget -q -O - http://nginx | grep "Hello"; done'
 
 echo "--- Test complete. Pod 'my-proxy' has been removed. ---"
